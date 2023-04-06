12b873252a7cb4499e238f093fbcb6d59264eef0
reiya0104 <reiya0104@ezweb.ne.jp>
2023-04-07 00:58:34 +0900

    feat: add Board component
    Add Board component to display the game board in the Revertile game.

--- lib/board.dart
+++ lib/board.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Board extends StatelessWidget {
-  const Board({super.key});
+  const Board({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // ボードを描画するコードを記述する
-    return Container();
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 8, // 横に8個のウィジェットを表示
+      ),
+      itemBuilder: (context, index) {
+        // TODO: ウィジェットを作成する
+        return Container();
+      },
+      itemCount: 64, // ウィジェットの数を指定
+    );
   }
 }
