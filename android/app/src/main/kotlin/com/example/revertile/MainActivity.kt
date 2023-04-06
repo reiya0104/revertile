1879b10096f73a7316c03cb244f34541dc0fafdc
reiya0104 <reiya0104@ezweb.ne.jp>
2023-04-07 00:51:10 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/revertile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.revertile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Revertile',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Revertile'),
+        ),
+        body: const Center(
+          child: Text('Hello, Revertile!'),
+        ),
+      ),
+    );
+  }
+}
