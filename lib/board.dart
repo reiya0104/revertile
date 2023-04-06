af39083146fa0955b4842195518f473f0abdf480
reiya0104 <reiya0104@ezweb.ne.jp>
2023-04-07 01:18:34 +0900

    feat: add pieces list to Board component
    Add pieces list to Board component to represent the game pieces on the game board in the Revertile game.

--- lib/board.dart
+++ lib/board.dart
@@ -7,17 +7,18 @@ class Board extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final pieces = List.generate(
+      64,
+      (index) => index % 2 == 0 ? PieceColor.black : PieceColor.white,
+    );
     return GridView.builder(
       itemCount: 64,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 8,
       ),
       itemBuilder: (context, index) {
-        // index に対応する駒の状態を取得するコードを記述する
-        // 以下は仮実装
-        final isBlack = index % 2 == 0;
         return Piece(
-          color: isBlack ? PieceColor.black : PieceColor.white,
+          color: pieces[index],
         );
       },
     );
