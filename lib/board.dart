f248916cd14813d89d742929e4ace65b7421a901
reiya0104 <reiya0104@ezweb.ne.jp>
2023-04-07 00:53:48 +0900

    feat: Board クラスの作成

---
+++ lib/board.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Board extends StatelessWidget {
+  const Board({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ボードを描画するコードを記述する
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'board.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Revertile'),
         ),
         body: const Center(
-          child: Text('Hello, Revertile!'),
+          child: Board(),
         ),
       ),
     );
